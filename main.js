01777653b0e7804aa4d350ee84e3d48b5ff0e63e
Aaron McClinchey <metzuda@protonmail.com>
2020-09-17 08:44:31 -0600

    several bug fixes: scoring more accurate; fixed bad color selection; prevented generated colors from going out of bounds

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let score = {
   firstGuessCorrect: 0,
 };
 
+let gameInProgress = true;
 let numSquares = 6;
 let veryHard = !true;
 let colors = [];
@@ -34,18 +35,24 @@ function init() {
   for (let i = 0; i < squares.length; i++) {
     // add click listeners to squares
     squares[i].addEventListener("click", function () {
-      score.clicks += 1;
+      if(!gameInProgress) return
+      
+      score.clicks += 1
       //grab color of clicked square
       const clickedColor = this.style.background;
-
+      
       if (clickedColor === pickedColor) {
+        
+        gameInProgress = !true
         score.gamesPlayed += 1;
-
-        if (score.averageGuesses != 0) {
-          score.averageGuesses = ((score.averageGuesses + score.clicks) / 2);
-        } else score.averageGuesses = score.clicks;
-
-        if (score.clicks == 1) score.firstGuessCorrect += 1;
+        
+        if(!gameInProgress){
+          if (score.averageGuesses != 0) {
+            score.averageGuesses = ((score.averageGuesses + score.clicks) / 2);
+          } else score.averageGuesses = score.clicks;
+  
+          if (score.clicks == 1) score.firstGuessCorrect += 1;
+        } 
         messageDisplay.textContent = "Correct!";
 
         changeColors(clickedColor);
@@ -64,6 +71,7 @@ function init() {
 
 function reset() {
   
+  gameInProgress = true
   score.clicks = 0
   // generate all new colors
   if(!veryHard) colors = generateRandomColors(numSquares)
@@ -124,7 +132,7 @@ const generateCloselyRandomColors = numSquares => {
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
   
-  let arr = [`rgb(${r},${g},${b})`]
+  let arr = [`rgb(${r}, ${g}, ${b})`]
     
   for (let i = 1; i < numSquares; i++) {
     
@@ -135,16 +143,20 @@ const generateCloselyRandomColors = numSquares => {
       let posNeg = (Math.random() * 2) > 1 ? -1 : 1
       let diff = posNeg * (Math.floor(Math.random() * 105))
       
+      let x = r + diff > 255 ? 255 : r + diff
+      let y = g + diff > 255 ? 255 : g + diff
+      let z = b + diff > 255 ? 255 : b + diff
+      
       switch(i){
         case 0:
-          color.push(Math.abs(r + diff));
+          color.push(Math.abs(x));
           break;
         case 1:
-          color.push(Math.abs(g + diff))
+          color.push(Math.abs(y))
           break;
         case 2:
         default:
-          color.push(Math.abs(b + diff))
+          color.push(Math.abs(z))
       }
     }
     
